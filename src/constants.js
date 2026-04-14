5a2762e682372cf0dec626c0b89cb1243a1952e2
Mohit Sharma <msharma89339@gmail.com>
2026-04-14 15:43:03 +0530

    Update URL to use latest Gemini model

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const URL= "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent" 
+export const URL= "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent" 
 export const API_KEY = "AIzaSyAKT1oTA9a2sMiOinH9hHMix7OpBDdKqcc"
